9da03d8ab567c0307807edb5cfa797be490f48eb
Brandy Moore <brandymoore923@gmail.com>
2022-08-22 22:19:40 -0500

    added basic html outline. Started css

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 var button = document.querySelector('.btn');
-var start = document.querySelector('#start');
+var start = document.querySelector('#intro');
 var quiz = document.querySelector('#quiz');
+//var end = document.querySelector('#end');
+//var submit = document.querySelector('#submit);
+
+//var questions = ["First", "Second", "Third", "Fourth", "Fifth"];
 
 const begin = function (){
     start.style.display= "none";
     quiz.style.display= "block";
-
+    //questions.style.display="block";
 }
-
 button.addEventListener("click", begin)
+
+
