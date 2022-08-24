77134c90a4cefd370bc0e58d25dbf34a2792026e
Brandy Moore <brandymoore923@gmail.com>
2022-08-23 21:18:28 -0500

    started new javaScript and outlined somewhat with comments

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+//html variables
+var startBtn = document.querySelector("#startBtn");
+var timerEl = document.querySelector("#timer");
+var timerEl2 = document.querySelector("header").children[1];
+var submitScoreBtn = document.querySelector("#submitBtn");
+var viewScoreBtn = document.querySelector("#scoresbtn");
+var clearScoreBtn = document.querySelector("#clearScores");
+var answerBtn = document.querySelector("ul");
+var returnHome = document.querySelector("#backBtn");
+var titleEl = document.querySelector("#title");
+
+//this is where I will put my questions and answers
+var questionEl = {
+    questions: [ //I will write my questions here
+
+    ],
+    answers: [//the answers will go here
+
+    ]
+}
+
+var gameTimer = 60; //this is the time for the game
+
+//this is where the time and score stuff will be written
+var cursor = 0
+var remTime = gameTimer; //the time left
+var score = 0;
+var gameDone = true;
+
+//intro to quiz will go here
+
+
+//when start button is hit this is what will go here
+
+//when time ends this is what will go here
+
+//
+
